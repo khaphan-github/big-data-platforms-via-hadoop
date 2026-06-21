3e95831afb9a64fa93fb7aa18eea07fabd3f237f
khaphan-github <phanhoangkha01@gmail.com>
2026-06-21 23:08:09 +0700

    feat: add JAR and model copying to submit script for keyword transformation

--- transform/scala/submit.sh
+++ transform/scala/submit.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 cd "$(cd "$(dirname "$0")" && pwd)"
+JAR_NAME="transform-keyword-version-10-56-21-06-2026"
+docker cp "$JAR_NAME.jar" spark-master:/tmp/scala-assembly.jar
+docker cp libs/vntokenizer4.1/models spark-master:/tmp/vnlp-models
 docker exec spark-master /opt/spark/bin/spark-submit \
   --master spark://spark-master:7077 \
   --deploy-mode client \
